934f383e099d5218f52ab7a906a7c74765e92569
Ben <bigriffin94@outlook.com>
2019-09-20 08:25:23 -0700

    PrivateRoute + AxiosWithAuth created.

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 import Login from "./components/Login";
 import "./styles.scss";
@@ -9,11 +9,18 @@ function App() {
   return (
     <Router>
       <div className="App">
+        <ul>
+          <li>
+            <Link to="/protected">Bubble Page</Link>
+          </li>
+        </ul>
+
         <Route exact path="/" component={Login} />
         {/* 
           Build a PrivateRoute component that will 
           display BubblePage when you're authenticated 
         */}
+        <PrivateRoute exact path="/protected" component={BubblePage} />
       </div>
     </Router>
   );
